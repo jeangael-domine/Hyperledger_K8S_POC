f8b9246e765d40dc2a338d27a2dca53e3305f5a3
m428823 <jgdomine@airfrance.fr>
2018-09-18 14:16:43 +0200

    Fix script

--- new-org/addNewOrgCryptoMaterial.sh
+++ new-org/addNewOrgCryptoMaterial.sh
@@ -2,13 +2,14 @@
 
 WORKING_DIR=/opt/gopath/src/github.com/hyperledger/fabric/peer/new-org
 CHANNEL_NAME=mychannel
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/node-port/orderers/orderer.node-port/msp/tlscacerts/tlsca.node-port-cert.pem
 
 cd $WORKING_DIR
 rm -f config_block.pb config.json config.pb modified_config.json modified_config.pb  org2.json org2_update_in_envelope.json org2_update_in_envelope.pb org2_update.json org2_update.pb
 cd -
 
 echo "===================== Fetching config of channel '$CHANNEL_NAME' ===================== "
-peer channel fetch config $WORKING_DIR/config_block.pb -o orderer-node-port:7050 -c $CHANNEL_NAME
+peer channel fetch config $WORKING_DIR/config_block.pb -o orderer-node-port:7050 -c $CHANNEL_NAME --cafile $ORDERER_CA
 echo
 
 echo "===================== Trimming channel '$CHANNEL_NAME' config and converting it into JSON ===================== "
@@ -36,7 +37,7 @@ configtxlator proto_decode --input $WORKING_DIR/org2_update.pb --type common.Con
 echo
 
 echo "===================== Wrapping the update file in an envelope message ===================== "
-echo '{"payload":{"header":{"channel_header":{"channel_id":"mychannel", "type":2}},"data":{"config_update":'$(cat $WORKING_DIR/org2_update.json)'}}}' | jq . > $WORKING_DIR/org2_update_in_envelope.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"$CHANNEL_NAME", "type":2}},"data":{"config_update":'$(cat $WORKING_DIR/org2_update.json)'}}}' | jq . > $WORKING_DIR/org2_update_in_envelope.json
 echo
 
 echo "===================== Converting the envelope message into the fully fledged protobuf format required by Fabric ===================== "
@@ -48,5 +49,5 @@ peer channel signconfigtx -f $WORKING_DIR/org2_update_in_envelope.pb
 echo
 
 echo "===================== Updating the config of channel '$CHANNEL_NAME' ===================== "
-peer channel update -f $WORKING_DIR/org2_update_in_envelope.pb -c $CHANNEL_NAME -o orderer-node-port:7050
+peer channel update -f $WORKING_DIR/org2_update_in_envelope.pb -c $CHANNEL_NAME -o orderer-node-port:7050 --cafile $ORDERER_CA
 echo
