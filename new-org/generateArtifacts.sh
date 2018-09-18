495506acf852f129ed82b153ebca14d9b96f3b6f
m428823 <jgdomine@airfrance.fr>
2018-09-18 11:07:26 +0200

    Org2: crypto-config output directory changed

--- new-org/generateArtifacts.sh
+++ new-org/generateArtifacts.sh
@@ -3,7 +3,7 @@
 WORKING_DIRECTORY=$HOME/Hyperledger_K8S_POC
 FABRIC_CFG_PATH=$WORKING_DIRECTORY 
 
-cryptogen generate --config=$WORKING_DIRECTORY/new-org/crypto-config.yaml
+cryptogen generate --config=$WORKING_DIRECTORY/new-org/crypto-config.yaml --output="$WORKING_DIRECTORY/new-org/crypto-config"
 
 configtxgen -printOrg Org2MSP > $WORKING_DIRECTORY/new-org/org2.json
 
